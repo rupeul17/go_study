a0fd767627c8df3963d9a64cd67d5836064cfe9f
rupeul17 <56832593+rupeul17@users.noreply.github.com>
2022-06-13 16:48:56 +0900

    학습 중 수정

--- src/section2/variable1.go
+++ src/section2/variable1.go
@@ -3,32 +3,32 @@ package main
 import "fmt"
 
 func main() {
-  // 기본 초기화
-  // 정수타입 : 0, 실수(소수점) : 0.0, 문자열 : "", Boolean : true, false
-  // 변수명 : 숫자 첫글자x, 대소문자 구분o, 문자, 숫자, 밑줄, 특수기호 사용 가능
-  // 변수 및 상수 : 함수 내외 사용 가능
-  var a int
-  var b string
-  var c, d, e int
-  var f, g, h int = 1, 2, 3
-  var i float32 = 11.4
-  var j string = "Hi! Golang!"
-  var k = 4.74
-  var l = "Hi! Seoul!"
-  var m = true
+	// 기본 초기화
+	// 정수타입 : 0, 실수(소수점) : 0.0, 문자열 : "", Boolean : true, false
+	// 변수명 : 숫자 첫글자x, 대소문자 구분o, 문자, 숫자, 밑줄, 특수기호 사용 가능
+	// 변수 및 상수 : 함수 내외 사용 가능
+	var a int
+	var b string
+	var c, d, e int
+	var f, g, h int = 1, 2, 3
+	var i float32 = 11.4
+	var j string = "Hi! Golang!"
+	var k = 4.74
+	var l = "Hi! Seoul!"
+	var m = true
 
-  fmt.Println("a : ". a)
-  fmt.Println("b : ", b)
-  fmt.Println("c : ", c)
-  fmt.Println("d : ", d)
-  fmt.Println("e : ", e)
-  fmt.Println("f : ", f)
-  fmt.Println("g : ", g)
-  fmt.Println("h : ", h)
-  fmt.Println("i : ", i)
-  fmt.Println("j : ", j)
-  fmt.Println("k : ", k)
-  fmt.Println("l : ", l)
-  fmt.Println("m : ", m)
+	fmt.Println("a : ", a)
+	fmt.Println("b : ", b)
+	fmt.Println("c : ", c)
+	fmt.Println("d : ", d)
+	fmt.Println("e : ", e)
+	fmt.Println("f : ", f)
+	fmt.Println("g : ", g)
+	fmt.Println("h : ", h)
+	fmt.Println("i : ", i)
+	fmt.Println("j : ", j)
+	fmt.Println("k : ", k)
+	fmt.Println("l : ", l)
+	fmt.Println("m : ", m)
 
 }
--- src/section2/variable2.go
+++ src/section2/variable2.go
@@ -3,18 +3,18 @@ package main
 import "fmt"
 
 func main() {
-  // 여러개 선언
-  var (
-    name string = "machine"
-    height int32
-    weight float32
-    isRunning bool
-  )
+	// 여러개 선언
+	var (
+		name      string = "machine"
+		height    int32
+		weight    float32
+		isRunning bool
+	)
 
-  height = 250
-  weight = 350.56
-  isRunning = true
+	height = 250
+	weight = 350.56
+	isRunning = true
 
-  fmt.Println("name : ", name, "height : ", height, "weight : ", weight, "isRunning : ", isRunning)
+	fmt.Println("name : ", name, "\n", "height : ", height, "\n", "weight : ", weight, "\n", "isRunning : ", isRunning)
 
 }
